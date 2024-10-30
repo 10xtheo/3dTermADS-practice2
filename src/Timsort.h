71bcf4952a2bab38bce6a97c51da5cf261b58e7a
theo <theoemail@internet.ru>
2024-10-30 18:40:40 +0300

    nicely working version no galloping

--- src/Timsort.h
+++ src/Timsort.h
@@ -3,33 +3,32 @@
 
 #include <vector>
 #include <iostream>
+#include <stack>
+#include <algorithm>
+
 using namespace std;
 
 class Timsort
 {
 private:
     int getMinrun(int n);
-    void insertionSort(vector<int> *run);
-    void mergeSort(vector<vector<int>> *runs);
 public:
     void timsort(vector<int> *list);
-    vector<vector<int>> slicer(vector<int> *list);
+    void insertionSort(unsigned begin, unsigned size, vector<int> *run);
+    std::stack<std::pair<size_t, size_t>> slicer(vector<int> *list);
+    void merge(vector<int> *list, std::pair<size_t, size_t> A, std::pair<size_t, size_t> B);
+    void mergeSort(vector<int> *list, std::stack<std::pair<size_t, size_t>> stack);
+    size_t binarySearch(const std::vector<int>& arr, int target, size_t start, size_t end);
 };
 
 void Timsort::timsort(vector<int> *list)
 {
-    int minrun = getMinrun(list->size());
-
+    stack<pair<size_t, size_t>> stack = slicer(list);
+    mergeSort(list, stack);
 }
-
 
 int Timsort::getMinrun(int n)
 {
-//    Наиболее эффективно использовать значения из диапазона (32;65).
-//    Исключение — если N < 64, тогда minrun = N и timsort превращается в
-//    простую сортировку вставкой. В данный момент алгоритм расчёта minrun
-//    просто до безобразия: берём старшие 6 бит из N и добавляем единицу,
-//    если в оставшихся младших битах есть хотя бы один ненулевой.
     int r = 0;
     while (n >= 64)
     {
@@ -39,52 +38,36 @@ int Timsort::getMinrun(int n)
     return n + r;
 }
 
-vector<vector<int>> Timsort::slicer(vector<int> *list)
+std::stack<std::pair<size_t, size_t>> Timsort::slicer(vector<int> *list)
 {
-    // TODO: FIX THAT KIDDO
     int minrun = getMinrun(list->size());
-    vector<vector<int>> result;
-    size_t i = 0;
-    while (i < list->size())
+    std::stack<std::pair<size_t, size_t>> result;
+    for (size_t i = 0; i < list->size(); i++)
     {
-        vector<int> cur;
-        for (;cur.size() < 2; ++i)
+        size_t start = i;
+        if ((*list)[i] <= (*list)[i + 1] && i + 1 < list->size())
         {
-            cur.push_back((*list)[i]);
-        }
-        if (cur[0] <= cur[1])
-        {
-            for (; (*list)[i] <= cur[cur.size() - 1] ; ++i)
-            {
-                cur.push_back((*list)[i]);
-            }
+            for (;(*list)[i] <= (*list)[i + 1] && i + 1 < list->size(); ++i);
         }
         else
         {
-            for (; (*list)[i] > cur[cur.size() - 1]; ++i)
-            {
-                cur.push_back((*list)[i]);
-            }
-            for (size_t j = 0; j < cur.size() / 2; ++j)
-            {
-                swap(cur[j], cur[cur.size() - 1 - j]);
-            }
+            for (;i + 1 < list->size() && (*list)[i] >= (*list)[i + 1]; ++i);
+            std::reverse(list->begin() + start, list->begin() + i + 1);
         }
-        for (; i < size_t(minrun); ++i)
-        {
-            cur.push_back((*list)[i]);
-        }
-        result.push_back(cur);
+        size_t runLength = i - start + 1;
+        for (; runLength < size_t(minrun) && i + 1 < list->size(); ++i, ++runLength);
+        insertionSort(start, runLength, list);
+        result.push({start, runLength});
     }
     return result;
 }
 
-void Timsort::insertionSort(vector<int> *run)
+void Timsort::insertionSort(unsigned begin, unsigned size, vector<int> *run)
 {
-    for (size_t j = 1; j < (*run).size(); ++j)
+    for (size_t j = begin+1; j < size+begin; ++j)
     {
         int value = (*run)[j];
-        for (size_t i = j - 1; i >= 0  && (*run)[i] > value; --i)
+        for (size_t i = j - 1; i >= begin && (*run)[i] > value; i--)
         {
             (*run)[i+1] = (*run)[i];
             (*run)[i] = value;
@@ -92,9 +75,106 @@ void Timsort::insertionSort(vector<int> *run)
     }
 }
 
-void Timsort::mergeSort(vector<vector<int>> *runs)
+void Timsort::mergeSort(vector<int> *list, std::stack<std::pair<size_t, size_t>> stack)
 {
+    while (stack.size() >= 3)
+    {
+        std::pair<size_t, size_t> X = stack.top();
+        stack.pop();
 
+        std::pair<size_t, size_t> Y = stack.top();
+        stack.pop();
+
+        std::pair<size_t, size_t> Z = stack.top();
+        stack.pop();
+
+        if (!(Z.second > Y.second + X.second) || !(Y.second > X.second))
+        {
+            std::pair<size_t, size_t> minXZ = (X.second < Z.second) ? X : Z;
+            std::pair<size_t, size_t> maxXZ = (X.second > Z.second) ? X : Z;
+            merge(list, Y, minXZ);
+            stack.push(maxXZ);
+            stack.push({std::min(Y.first, minXZ.first), Y.second+minXZ.second});
+        }
+    }
+    if (stack.size() == 2)
+    {
+        std::pair<size_t, size_t> X = stack.top();
+        stack.pop();
+
+        std::pair<size_t, size_t> Y = stack.top();
+        stack.pop();
+
+        merge(list, X, Y);
+    }
+}
+
+void Timsort::merge(vector<int> *list, std::pair<size_t, size_t> A, std::pair<size_t, size_t> B) {
+    // Определяем начальные и конечные индексы для обоих диапазонов
+
+    if (A.first > B.first)
+        swap(A, B);
+
+    size_t startA = A.first;
+    size_t endA = A.first + A.second;
+    size_t startB = B.first;
+    size_t endB = B.first + B.second;
+
+    // Создаем временный вектор для хранения объединенных результатов
+
+    vector<int> temporary;
+
+    // Заполняем временный вектор
+
+    for (size_t i = startA; i < endA; ++i)
+    {
+        temporary.push_back((*list)[i]);
+    }
+    size_t indexA = 0;  // Индекс для временного вектора
+    size_t indexB = startB;  // Индекс для второго диапазона
+    size_t i = startA;  // Индекс для записи в оригинальный список
+
+    // Слияние двух диапазонов
+    while (indexA < temporary.size() && indexB < endB)
+    {
+        if (temporary[indexA] <= (*list)[indexB])
+        {
+            (*list)[i++] = temporary[indexA++];
+        }
+        else
+        {
+            (*list)[i++] = (*list)[indexB++];
+        }
+    }
+
+    // Копируем оставшиеся элементы из временного вектора, если они есть
+    while (indexA < temporary.size())
+    {
+        (*list)[i++] = temporary[indexA++];
+    }
+
+    // Копируем оставшиеся элементы из второго диапазона, если они есть
+    while (indexB < endB)
+    {
+        (*list)[i++] = (*list)[indexB++];
+    }
+}
+
+size_t Timsort::binarySearch(const std::vector<int>& arr, int target, size_t start, size_t end)
+{
+    while (start < end)
+    {
+        size_t mid = start + (end - start) / 2;
+        if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid;
+        }
+    }
+    return start; // Возвращаем позицию для вставки
 }
 
 #endif // TIMSORT_H
--- src/application.cpp
+++ src/application.cpp
@@ -2,22 +2,22 @@
 #include "Timsort.h"
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 int Application::exec()
 {
     Timsort ts;
-    vector<int> list =
-    {5, 4, 3, 2, 8, 7, 9, };
-    vector<vector<int>> slices = ts.slicer(&list);
-    for (size_t i = 0; i < slices.size(); ++i)
+    vector<int> list = {7, 18, 21, 34, 1, 6, 7, 14, 5, 23, 41, 88, 0, 2, 33, 49, 54, 61, 76, 82, 9, 19, 25, 31, 38, 47, 52, 58, 65, 70, 74, 80, 86, 92, 97, 4, 10, 17, 22, 29, 36, 43, 50, 33, 49, 54, 61, 76, 82, 9, 19, 25, 31, 38, 47, 52, 58, 65, 70, 74, 80, 86, 92, 97, 4, 10, 17, 22, 29, 36, 43, 50, 57, 64, 71, 78, 83, 90, 5, 28, 37, 42, 56, 73, 89, 12, 34, 67, 5, 23, 41, 88, 0, 2, 33, 49, 54, 61, 76, 82, 9, 19, 25, 31, 38, 47, 52, 58, 65, 70, 74, 80, 86, 92, 97, 4, 10, 17, 22, 29, 36, 43, 50, 57, 64, 71, 78, 83, 90};
+
+    ts.timsort(&list);
+
+    for (size_t i = 0; i < list.size(); ++i)
     {
-        for (size_t j = 0; j < (slices[i]).size(); ++j)
-        {
-            cout << slices[i][j] << "x";
-        }
-        cout << "a";
+        cout << list[i] << " ";
     }
+    cout << endl;
+    cout << endl;
 
     return 0;
 }
